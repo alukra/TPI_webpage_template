386e95fc2a17d7829c9b6229183c344f53079b50
Angel Peraza <a.peraza@outlook.com>
2019-10-21 06:09:48 -0600

    preguntas frecuentes

---
+++ preguntas.php
@@ -0,0 +1,49 @@
+<?php
+    $title = "Preguntas frecuentes";
+    $mode = "light";
+
+    $content = '<div id="inSlider" class="carousel carousel-fade" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <div class="header-back eleven"></div>
+            </div>
+        </div>
+    </div>
+
+    
+<section class="features">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <div class="navy-line"></div>
+            <h1>Tienes dudas, te las aclararemos</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-5 col-lg-offset-1 features-text">
+            <h2>¿Cómo se realiza?</h2>
+            <i class="fa fa-gavel big-icon pull-right"></i>
+            <p>Para mayor información del proceso, te recomendamos vayas a nuestros <a href="./galeriavideos.php" >videos</a> </p>
+        </div>
+        <div class="col-lg-5 features-text">
+            <h2>¿Qué es el sublimado?</h2>
+            <i class="fa fa-bolt big-icon pull-right"></i>
+            <p>es una técnica de impresión digital que te permite crear regalos personalizados de modo muy sencillo.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-5 col-lg-offset-1 features-text">
+            <h2>¿Cuánto cuesta una taza sublimada? </h2>
+            <i class="fa fa-credit-card big-icon pull-right"></i>
+            <p>El costo de una taza es de $10 USD.</p>
+        </div>
+        <div class="col-lg-5 features-text">
+            <h2>Deseo un diseño en específico </h2>
+            <i class="fa fa-edit big-icon pull-right"></i>
+            <p>Si deseas un diseño especifico con gusto lo realizamos, puedes contactarnos en nuestras redes sociales, o en  <a href="./contacto.php" >contactos</a> .</p>
+        </div>
+    </div>
+</div>';
+    require_once("layout/template.php");
+    
+?>
