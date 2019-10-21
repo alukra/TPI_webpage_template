230f08b1eea4a7b0ba9f93cc4e107571cad8565c
Angel Peraza <a.peraza@outlook.com>
2019-10-21 03:51:48 -0600

    Contacto y quienes somos

---
+++ contacto.php
@@ -0,0 +1,49 @@
+<?php
+    $title = "Contactanos";
+    $mode = "light";
+
+    $content = '<div id="inSlider" class="carousel carousel-fade" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <div class="header-back four"></div>
+            </div>
+        </div>
+    </div>
+
+    <section class="features">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="navy-line"></div>
+                <h1>Contactanos</h1>
+                <h3>Tus opiniones, sugerencias y consultas nos interesan</h3>
+                <p>Print Cups esta atento a sus clientes, por tanto te brinda la oportunidad de contactarnos usando el siguiente formulario. No dudes en preguntarnos
+                datos sobre nuestros productos o sugerencias para un diseño, nosotros tomamos muy enserio el trabajo, buscamos dar la mejor atención posible y brindar productos de calidad.
+                </p>
+                <p>Responderemos a tu mensaje en la mayor brevedad posible y te invitamos a que tambien nos contactes por medio de nuestras redes sociales.
+                </P>
+            </div>
+            <div class="col-lg-12 text-center">
+                <div class="navy-line"></div>
+                <form  name="formContacto" method="post" action="./php/contacto.php">
+                
+                <label for="nombre" class="labelContacto">Nombre</label>
+                <input type="text" id="nombre" name="nombre" data-contacto="Nombre Cliente" placeholder="Digite su nombre" class="camposContacto" required>
+                
+                <label for="correo" class="labelContacto">Correo Electrónico</label>
+                <input type="email" id="correo" name="correo" data-contacto="Correo Cliente" placeholder="Digite su correo electrónico" class="camposContacto" required>
+        
+                <label for="comentario" class="labelContacto">Comentario/Sugerencia</label>
+                <textarea id="comentario" name="comentario" data-contacto="Comentario, Sugerencia Cliente" placeholder="Digite su comentario/sugerencia" class="camposContacto textareaContacto" required></textarea>
+                
+                <div class="centrado">
+                    <input type="submit" name="enviar" value="Enviar" class="botonContacto">
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    </section>';
+    require_once("layout/template.php");
+    
+?>
---
+++ quienessomos.php
@@ -0,0 +1,45 @@
+<?php
+    $title = "Quienes somos";
+    $mode = "light";
+
+    $content = '<div id="inSlider" class="carousel carousel-fade" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <div class="header-back three"></div>
+            </div>
+        </div>
+    </div>
+
+    <section class="features">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="navy-line"></div>
+                <h1>Acerca de nosotros</h1>
+                <p>¿Por qué no tomar tu bebida favorita en una taza personalizada o regalarle un detalle a una persona especial o un amigo? 
+                No hay razón para no hacerlo, Print Cups esta a tu servicio y podrás revisar nuestro catálogo de productos o si lo prefieres puedes realizar tu propio diseño y enviarlo para ser subliminado.
+                Contamos con herramientas y las técnicas adecuadas para brindar un producto de calidad y estamos atentos a cualquier sugerencia que nuestros clientes puedan darnos.
+                La entrega de tu producto esta garantizada y nos encargaremos de hacerla llegar hasta el lugar que nos indiques.</p>
+            </div>
+        </div>
+        <div class="row features-block">
+            <div class="col-lg-3 features-text wow fadeInLeft">
+                <small>Mision</small>
+                <p>Print Cups es un negocio de sublimación dedicado a ofrecer una alternativa en cuanto 
+                a la forma en que se toman las bebidas, dejando a un lado el diseño clásico de tazas, termos
+                 y botellas. Cada persona tiene una forma de expresión única y esta es reflejada hasta en los pequeños detalles. 
+                </p>
+            </div>
+            <div class="col-lg-6 text-right m-t-n-lg wow zoomIn">
+                <img src="img/termo.png" class="img-responsive" alt="Termo">
+            </div>
+            <div class="col-lg-3 features-text text-right wow fadeInRight">
+                <small>Vision</small>
+                <p>Proporcionar a nuestros clientes productos subliminados que satisfagan sus expectativas e imaginación y estén a su alcance sin dejar de lado la calidad de estos.</p>
+            </div>
+        </div>
+    </div>
+    </section>';
+    require_once("layout/template.php");
+    
+?>
